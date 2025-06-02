fca9584d7ce2506a24f0f7fe4daa06a1724efaec
ayushkothari1 <pahadiayush61@gmail.com>
2025-06-03 01:12:15 +0530

    list

---
+++ script.js
@@ -0,0 +1,17 @@
+const listEl = document.querySelectorAll(".list");
+const sectionEl = document.querySelectorAll(".section");
+const welcome = document.querySelector(".welcome");
+
+listEl.forEach(function (list, index) {
+  list.addEventListener("click", function (e) {
+    listEl.forEach(function (innerTab) {
+      innerTab.classList.remove("list-active");
+    });
+    welcome.style.display = "none";
+    list.classList.add("list-active");
+    sectionEl.forEach(function (section) {
+      section.classList.remove("active-section");
+    });
+    sectionEl[index].classList.add("active-section");
+  });
+});
